4838f4f8fc1d138d893bd2c0bef1f63ca9f7ef72
xFlody <86113026+xFlody@users.noreply.github.com>
2025-04-07 11:25:54 +0200

    v5

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,39 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('moje-cache').then(cache => {
-        return cache.addAll(['./', './index.html', './style.css']);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => {
-        return response || fetch(e.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = "v5";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css"
+];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
